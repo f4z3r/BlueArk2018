55fb6fa8d3c1b5ac49c993363a89bbc9d1c0877e
Julien Richard-Foy <julien@richard-foy.fr>
2018-11-10 10:03:04 +0100

    Revert "initiating the graph to equations work"
    This reverts commit 84c734baa54f2861a0444cf8d8ed662f8fc196a3.

--- blueark/model/fixtures.py
+++ blueark/model/fixtures.py
@@ -1,11 +1,12 @@
 import blueark.model.entities as entities
 
-src = entities.Source(30, False)
-srcToTank = entities.Pipe([src], 300, 0, 300)
-tank = entities.Tank([srcToTank], 2500, 1000)
-left = entities.Pipe([tank], 300, 0, 300)
-consumer1 = entities.Consumer([left], 100)
-right = entities.Pipe([tank], 200, 0.5, 300)
-consumer2 = entities.Consumer([left, right], 100)
+consumer1 = entities.Consumer(200)
+consumer2 = entities.Consumer(150)
+right = entities.Pipe(consumer2, 200, 0.5, 300)
+left = entities.Pipe(consumer2, 300, 0, 300)
+tank = entities.Tank([left, right], 250000, 10000)
+tankToSrc = entities.Pipe(tank, 300, 0, 300)
+src = entities.Source(tankToSrc, 100, False)
 
-graph = consumer2
+graph = src
+
