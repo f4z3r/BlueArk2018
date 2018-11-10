1dadc708bf3ffe6549c90a2a426982d34ab29c19
Petur <peture@student.ethz.ch>
2018-11-10 22:10:38 +0100

    fstream

--- blueark/optmization/main.cpp
+++ blueark/optmization/main.cpp
@@ -4,6 +4,7 @@
 #include <CGAL/QP_functions.h>
 #include <CGAL/Gmpq.h>
 #include <vector>
+#include <fstream>
 #include <string>
 
 typedef CGAL::Gmpq ET;
@@ -14,7 +15,8 @@ typedef CGAL::Quadratic_program_solution<ET> Solution;
 using std::cin;
 using std::cout;
 
-int main(){
+int main()
+{
   int n, m;
 
   std::cin >> n >> m;
@@ -22,15 +24,18 @@ int main(){
   Program lp(CGAL::EQUAL, true, 0, false, 0);
   double a, b, c, eq;
 
-  for(int var = 0; var < n; ++var){
+  for (int var = 0; var < n; ++var)
+  {
     // a <= var <= b
     cin >> a >> b >> names[var] >> c;
 
     lp.set_c(var, -c);
-    if(a > 0){
+    if (a > 0)
+    {
       lp.set_l(var, true, a);
     }
-    if(b >= 0){
+    if (b >= 0)
+    {
       lp.set_u(var, true, b);
     }
   }
@@ -39,29 +44,38 @@ int main(){
   // a[i][0] ... a[i][n-1] <= b[i]
   // where "<=" is an integer, 0 if equal
 
-  for(int constraint = 0; constraint < m; ++constraint){
-    for(int variable = 0; variable < n; ++variable){
+  for (int constraint = 0; constraint < m; ++constraint)
+  {
+    for (int variable = 0; variable < n; ++variable)
+    {
       cin >> a;
       lp.set_a(variable, constraint, a);
     }
     cin >> eq >> b;
     lp.set_b(constraint, b);
-    if(eq == 1){
+    if (eq == 1)
+    {
       lp.set_r(constraint, CGAL::SMALLER);
     }
   }
 
+  std::ofstream file;
+  file.open("output.txt", std::ofstream::out | std::ofstream::app);
+
   Solution s = CGAL::solve_linear_program(lp, ET());
-  if(!s.solves_linear_program(lp) || s.status() == CGAL::QP_INFEASIBLE)
-    std::cout << "no\n";
-  else{
+  if (!s.solves_linear_program(lp) || s.status() == CGAL::QP_INFEASIBLE)
+    file << "no\n";
+  else
+  {
     ET ans = s.objective_value().numerator() / s.objective_value().denominator();
-    std::cout << -CGAL::to_double(s.objective_value()) << "\n";
+    file << -CGAL::to_double(s.objective_value()) << "\n";
   }
-  CGAL::Quadratic_program_solution<ET>::Variable_value_iterator 
-    opt = s.variable_values_begin(); 
-  for(int i = 0; i < n; ++i){
-    cout << names[i] << "," << CGAL::to_double(*(opt + i)) << "\n";
+  CGAL::Quadratic_program_solution<ET>::Variable_value_iterator
+      opt = s.variable_values_begin();
+  for (int i = 0; i < n; ++i)
+  {
+    file << names[i] << "," << CGAL::to_double(*(opt + i)) << "\n";
   }
+  file.close();
   return 0;
 }
